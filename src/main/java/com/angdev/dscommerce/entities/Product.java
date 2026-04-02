e2e470f1bec2013e04152d4e199a9888f7f6b12d
Maria Luzia Monção Carneiro do Nascimento <luzia@MacBook-Air-de-Maria.local>
2026-04-02 17:41:16 -0300

    OrderItem, OrderItemPK

--- src/main/java/com/angdev/dscommerce/entities/Product.java
+++ src/main/java/com/angdev/dscommerce/entities/Product.java
@@ -3,6 +3,7 @@ package com.angdev.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -84,5 +85,11 @@ public class Product {
         this.imgUrl = imgUrl;
     }
 
+    public Set<OrderItem> getItems() {
+        return items;
+    }
 
+    public List<Order> getOrders(){
+        return items.stream().map(x -> x.getOrder()).toList();
+    }
 }
