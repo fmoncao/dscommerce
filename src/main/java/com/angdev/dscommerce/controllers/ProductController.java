f7b310ef78e5c97a5b0cd4d0bdcad1104eba606f
Maria Luzia Monção Carneiro do Nascimento <luzia@MacBook-Air-de-Maria.local>
2026-04-13 17:01:31 -0300

    Get product by id

---
+++ src/main/java/com/angdev/dscommerce/controllers/ProductController.java
@@ -0,0 +1,27 @@
+package com.angdev.dscommerce.controllers;
+
+import com.angdev.dscommerce.dto.ProductDTO;
+import com.angdev.dscommerce.entities.Product;
+import com.angdev.dscommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id){
+        ProductDTO dto = service.findById(id);
+        return dto;
+
+    }
+}
---
+++ src/main/java/com/angdev/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.angdev.dscommerce.repositories;
+
+import com.angdev.dscommerce.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
