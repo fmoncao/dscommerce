fabeb47747f773b11eac772231b717cd8cb52d8a
Maria Luzia Monção Carneiro do Nascimento <luzia@MacBook-Air-de-Maria.local>
2026-04-13 22:56:48 -0300

    GET Products paged

--- src/main/java/com/angdev/dscommerce/services/ProductService.java
+++ src/main/java/com/angdev/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.angdev.dscommerce.dto.ProductDTO;
 import com.angdev.dscommerce.entities.Product;
 import com.angdev.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,6 +23,13 @@ public class ProductService {
         Optional<Product> result = repository.findById(id);
         Product product = result.get();
         ProductDTO dto = new ProductDTO(product);
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> result = repository.findAll(pageable);
+        Page<ProductDTO> dto = result.map(x -> new ProductDTO(x));
         return dto;
     }
 
